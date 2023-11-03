79d4c2dc8d5d764396067018e06050d1665f3da1
Rui Silveira <93665100+ruipedro84@users.noreply.github.com>
2023-11-03 17:59:30 +0000

    Add files via upload

---
+++ waldmeister.pl
@@ -0,0 +1,74 @@
+% Definição de fatos e regras
+
+% Define os jogadores
+players([player1, player2]).
+
+% Define as alturas das árvores
+heights([baixo, medio, alto]).
+
+% Define as cores das árvores
+colors([verde_claro, verde, verde_escuro]).
+
+% Define as combinações de altura e cor
+combinations([(baixo, verde_claro), (baixo, verde), (baixo, verde_escuro),
+              (medio, verde_claro), (medio, verde), (medio, verde_escuro),
+              (alto, verde_claro), (alto, verde), (alto, verde_escuro)]).
+
+% Inicializa o estado inicial do jogo
+initial_state([player1, player2], [(P1Height, P1Color), (P2Height, P2Color)]) :- 
+    random_member(P1Height, [baixo, medio, alto]),
+    random_member(P1Color, [verde_claro, verde, verde_escuro]),
+    random_member(P2Height, [baixo, medio, alto]),
+    random_member(P2Color, [verde_claro, verde, verde_escuro]).
+
+% Predicado para mover uma árvore
+move([Player, State, Reserve], From, To, NewState) :-
+    select((Height, Color), State, TempState),
+    select((NewHeight, NewColor), Reserve, TempReserve),
+    append(TempState, [(NewHeight, NewColor)], NewBoard),
+    append(TempReserve, [(Height, Color)], NewReserve),
+    NewState = [Player, NewBoard, NewReserve].
+
+% Predicado para verificar se um jogador venceu
+check_winner([_, State, _], Player) :- 
+    count_groups(State, Player, Groups),
+    max_list(Groups, MaxGroups),
+    nth1(Player, Groups, MaxGroups).
+
+% Predicado para contar grupos de acordo com a altura ou cor
+count_groups(State, Player, Groups) :-
+    heights(Heights),
+    colors(Colors),
+    combinations(Combinations),
+    count_groups_helper(Heights, State, Player, HeightGroups),
+    count_groups_helper(Colors, State, Player, ColorGroups),
+    append(HeightGroups, ColorGroups, AllGroups),
+    count_combinations(Combinations, AllGroups, Groups).
+
+count_groups_helper([], _, _, []).
+count_groups_helper([H|T], State, Player, [Count|Rest]) :-
+    count_height_or_color(H, State, Player, Count),
+    count_groups_helper(T, State, Player, Rest).
+
+count_height_or_color(_, [], _, 0).
+count_height_or_color(Target, [(Height, Color)|Rest], Player, Count) :-
+    (Player = 1, Height = Target; Player = 2, Color = Target),
+    count_height_or_color(Target, Rest, Player, NextCount),
+    Count is NextCount + 1;
+    count_height_or_color(Target, Rest, Player, Count).
+
+count_combinations([], _, []).
+count_combinations([(Height, Color)|Rest], Groups, [Count|Result]) :-
+    count_combination(Height, Color, Groups, Count),
+    count_combinations(Rest, Groups, Result).
+
+count_combination(Height, Color, Groups, Count) :-
+    count_combination_helper(Height, Color, Groups, 0, Count).
+
+count_combination_helper(_, _, [], Count, Count).
+count_combination_helper(Height, Color, [(H, C)|Rest], Acc, Count) :-
+    ((Height = H; Color = C) -> NextAcc is Acc + 1; NextAcc is Acc),
+    count_combination_helper(Height, Color, Rest, NextAcc, Count).
+
+% Exemplo de uso:
+% ?- initial_state(State), move(State, (medio, verde_claro), (alto, verde_escuro), NewState).
